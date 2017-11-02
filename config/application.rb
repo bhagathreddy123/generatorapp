7831560b1868846791226bc405785f3e77b32537
bhagath <bhagath@getcustomexposure.com>
2017-11-02 14:45:26 +0530

    custom confuguration for mangaing generators

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,13 @@ Bundler.require(*Rails.groups)
 
 module Generatorapp
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+      config.generators do |g|
+        g.orm :active_record
+        g.template_engine :erb
+        g.test_framework :test_unit, fixture: false
+        g.stylesheets false
+        g.javascripts true
+      end
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102081221) do
+ActiveRecord::Schema.define(version: 20171102090057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
